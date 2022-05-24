b1bff0bbbed07386fcc9f5689863847e5a3dae2f
ClusterH <mycluster1990@gmail.com>
2022-05-24 11:51:10 -0500

    chore(): Update api base link

--- src/config/constants/baseURLs.ts
+++ src/config/constants/baseURLs.ts
@@ -2,9 +2,6 @@ export const BASE_IPFS_URL = 'https://ipfs.io/ipfs/'
 
 export const SIGNATURE_RELAY_API_URL = 'https://us-central1-patchwork-canteen.cloudfunctions.net/app/signature'
 
-export const API_URL =
-  process.env.REACT_APP_ENVIRONMENT === 'prod'
-    ? 'https://ura-creator-backend-prod-iw2xtmvktq-uc.a.run.app/'
-    : 'https://ura-creator-backend-dev-iw2xtmvktq-uc.a.run.app/'
+export const API_URL = process.env.REACT_APP_ENVIRONMENT === 'prod' ? 'https://api.choids.xyz/' : 'https://dev.api.choids.xyz/'
 
 export const OPENSEA_LINK = {}
